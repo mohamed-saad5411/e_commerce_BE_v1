c1e4fad9b195dbde3144e32912c3413c2a0718e8
mohamed-saad5411 <124407370+mohamed-saad5411@users.noreply.github.com>
2025-06-16 02:39:29 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import { dbConnection } from './database/dbConnection.js'
+const app = express()
+const port = 3000
+
+
+
+
+
+
+
+
+
+
+
+
+// app.use(express.json())
+dbConnection()
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
